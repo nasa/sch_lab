32512e80df744f67dafe1ec4a10c01b7b3b7145b
avan989 <avan989@yahoo.com>
2019-10-02 08:31:45 -0400

    fix #11, use cFS table service

--- fsw/platform_inc/sch_lab_sched_tab.h
+++ fsw/platform_inc/sch_lab_sched_tab.h
@@ -53,49 +53,22 @@
 */
 #define SCH_LAB_END_OF_TABLE          0
 #define SCH_LAB_MAX_SCHEDULE_ENTRIES  32
+#define SCH_TBL_DEFAULT_FILE          "/cf/sch_lab_table.tbl"
 
 /*
 ** Typedefs
 */
 typedef struct
 {
-    CFE_SB_MsgId_t  MessageID;  /* Message ID for the table entry */
-    uint32          PacketRate; /* Rate: Send packet every N seconds */
-    uint32          Counter;    /* Counter used to send packet */
+    CFE_SB_MsgId_t  MessageID[SCH_LAB_MAX_SCHEDULE_ENTRIES];  /* Message ID for the table entry */
+    uint32          PacketRate[SCH_LAB_MAX_SCHEDULE_ENTRIES]; /* Rate: Send packet every N seconds */
+    uint32          Counter[SCH_LAB_MAX_SCHEDULE_ENTRIES];    /* Counter used to send packet */
      
 } SCH_LAB_ScheduleTable_t;
 
-
 /*
-** SCH Lab schedule table
-** When populating this table:
-**  1. Make sure the table is terminated by the SCH_LAB_END_OF_TABLE entry
-**  2. You can have commented out entries, but a zero MID will terminate the table processing, 
-**      skipping the remaining entries.
-**  3. If the table grows too big, increase SCH_LAB_MAX_SCHEDULE_ENTRIES
+** Local Function Prototypes
 */
-SCH_LAB_ScheduleTable_t SCH_LAB_ScheduleTable[SCH_LAB_MAX_SCHEDULE_ENTRIES] = 
-{ 
-     { CFE_ES_SEND_HK_MID,   4, 0 },  /* Housekeeping requests go out every 4 seconds */
-     { CFE_EVS_SEND_HK_MID,  4, 0 },
-     { CFE_TIME_SEND_HK_MID, 4, 0 },
-     { CFE_SB_SEND_HK_MID,   4, 0 },
-     { CFE_TBL_SEND_HK_MID,  4, 0 },
-     
-     { CI_LAB_SEND_HK_MID,   4, 0 },
-     { TO_LAB_SEND_HK_MID,   4, 0 },
-     { SAMPLE_APP_SEND_HK_MID,   4, 0 },
 
-#if 0
-     { SC_SEND_HK_MID,       4, 0 },
-     { SC_1HZ_WAKEUP_MID,    1, 0 },  /* Example of a 1hz packet */
-     { HS_SEND_HK_MID,       4, 0 },
-     { FM_SEND_HK_MID,       4, 0 },
-     { DS_SEND_HK_MID,       4, 0 },
-     { LC_SEND_HK_MID,       4, 0 },
-
-#endif
-
-     { SCH_LAB_END_OF_TABLE, 0, 0 }
-};
-
+int32 SCH_LAB_TblValidation(void *MySchTBL);
+int32 SCH_LAB_AppInit(void);
