0fd023b0a0110734dba4148b45f2d89d5c5a6be7
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-10-06 14:06:46 -0400

    Fix #150, implement header file convention for SCH_LAB
    Move configurable header files into a separate directory with names
    according to the pattern documented in CFE.

--- fsw/tables/sch_lab_table.c
+++ fsw/tables/sch_lab_table.c
@@ -17,7 +17,7 @@
  ************************************************************************/
 
 #include "cfe_tbl_filedef.h" /* Required to obtain the CFE_TBL_FILEDEF macro definition */
-#include "sch_lab_table.h"
+#include "sch_lab_tbl.h"
 #include "cfe_sb.h" /* Required to use the CFE_SB_MSGID_WRAP_VALUE macro */
 
 #ifdef HAVE_CI_LAB
@@ -52,15 +52,6 @@
 #include "lc_msgids.h"
 #endif
 
-
-/*
-** Include headers for message IDs here
-*/
-#include "ci_lab_msgids.h"
-#include "to_lab_msgids.h"
-
-#include "sample_app_msgids.h"
-
 /*
 ** SCH Lab schedule table
 ** When populating this table:
@@ -70,43 +61,44 @@
 **  3. If the table grows too big, increase SCH_LAB_MAX_SCHEDULE_ENTRIES
 */
 
-SCH_LAB_ScheduleTable_t SCH_TBL_Structure = {.TickRate = 100,
-                                             .Config   = {
-                                                 {CFE_SB_MSGID_WRAP_VALUE(CFE_ES_SEND_HK_MID), 100, 0},   /* Example of a 1hz packet */
-                                                 {CFE_SB_MSGID_WRAP_VALUE(CFE_TBL_SEND_HK_MID), 50, 0},
-                                                 {CFE_SB_MSGID_WRAP_VALUE(CFE_TIME_SEND_HK_MID), 98, 0},
-                                                 {CFE_SB_MSGID_WRAP_VALUE(CFE_SB_SEND_HK_MID), 97, 0},
-                                                 {CFE_SB_MSGID_WRAP_VALUE(CFE_EVS_SEND_HK_MID), 96, 0},
+SCH_LAB_ScheduleTable_t SCH_LAB_ScheduleTable = {
+    .TickRate = 100,
+    .Config   = {
+        {CFE_SB_MSGID_WRAP_VALUE(CFE_ES_SEND_HK_MID), 100, 0}, /* Example of a 1hz packet */
+        {CFE_SB_MSGID_WRAP_VALUE(CFE_TBL_SEND_HK_MID), 50, 0},
+        {CFE_SB_MSGID_WRAP_VALUE(CFE_TIME_SEND_HK_MID), 98, 0},
+        {CFE_SB_MSGID_WRAP_VALUE(CFE_SB_SEND_HK_MID), 97, 0},
+        {CFE_SB_MSGID_WRAP_VALUE(CFE_EVS_SEND_HK_MID), 96, 0},
 
-                                            /* Example of including additional open source apps  */
-                                            #ifdef HAVE_CI_LAB
-                                                 {CFE_SB_MSGID_WRAP_VALUE(CI_LAB_SEND_HK_MID), 95, 0},
-                                            #endif
-                                            #ifdef HAVE_TO_LAB
-                                                 {CFE_SB_MSGID_WRAP_VALUE(TO_LAB_SEND_HK_MID), 94, 0},
-                                            #endif
-                                            #ifdef HAVE_SAMPLE_APP
-                                                 {CFE_SB_MSGID_WRAP_VALUE(SAMPLE_APP_SEND_HK_MID), 93, 0},
-                                            #endif
-                                            #ifdef HAVE_SC
-                                                 {CFE_SB_MSGID_WRAP_VALUE(SC_SEND_HK_MID), 92, 0},
-                                                 {CFE_SB_MSGID_WRAP_VALUE(SC_1HZ_WAKEUP_MID), 91, 0},
-                                            #endif
-                                            #ifdef HAVE_HS
-                                                 {CFE_SB_MSGID_WRAP_VALUE(HS_SEND_HK_MID), 90, 0}, /* Example of a message that wouldn't be sent */
-                                            #endif
-                                            #ifdef HAVE_FM
-                                                 {CFE_SB_MSGID_WRAP_VALUE(FM_SEND_HK_MID), 101, 0},
-                                            #endif
-                                            #ifdef HAVE_DS
-                                                 {CFE_SB_MSGID_WRAP_VALUE(DS_SEND_HK_MID), 102, 0},
-                                            #endif
-                                            #ifdef HAVE_LC
-                                                {CFE_SB_MSGID_WRAP_VALUE(LC_SEND_HK_MID), 103, 0},
-                                                {CFE_SB_MSGID_WRAP_VALUE(LC_SAMPLE_AP_MID),     500, 0, 8, { 0, 175, 1 }},
-                                            #endif
+/* Example of including additional open source apps  */
+#ifdef HAVE_CI_LAB
+        {CFE_SB_MSGID_WRAP_VALUE(CI_LAB_SEND_HK_MID), 95, 0},
+#endif
+#ifdef HAVE_TO_LAB
+        {CFE_SB_MSGID_WRAP_VALUE(TO_LAB_SEND_HK_MID), 94, 0},
+#endif
+#ifdef HAVE_SAMPLE_APP
+        {CFE_SB_MSGID_WRAP_VALUE(SAMPLE_APP_SEND_HK_MID), 93, 0},
+#endif
+#ifdef HAVE_SC
+        {CFE_SB_MSGID_WRAP_VALUE(SC_SEND_HK_MID), 92, 0},
+        {CFE_SB_MSGID_WRAP_VALUE(SC_1HZ_WAKEUP_MID), 91, 0},
+#endif
+#ifdef HAVE_HS
+        {CFE_SB_MSGID_WRAP_VALUE(HS_SEND_HK_MID), 90, 0}, /* Example of a message that wouldn't be sent */
+#endif
+#ifdef HAVE_FM
+        {CFE_SB_MSGID_WRAP_VALUE(FM_SEND_HK_MID), 101, 0},
+#endif
+#ifdef HAVE_DS
+        {CFE_SB_MSGID_WRAP_VALUE(DS_SEND_HK_MID), 102, 0},
+#endif
+#ifdef HAVE_LC
+        {CFE_SB_MSGID_WRAP_VALUE(LC_SEND_HK_MID), 103, 0},
+        {CFE_SB_MSGID_WRAP_VALUE(LC_SAMPLE_AP_MID), 500, 0, 8, {0, 175, 1}},
+#endif
 
-                                             }};
+    }};
 
 /*
 ** The macro below identifies:
@@ -115,4 +107,4 @@ SCH_LAB_ScheduleTable_t SCH_TBL_Structure = {.TickRate = 100,
 **    3) a brief description of the contents of the file image
 **    4) the desired name of the table image binary file that is cFE compatible
 */
-CFE_TBL_FILEDEF(SCH_TBL_Structure, SCH_LAB_APP.SCH_LAB_SchTbl, Schedule Lab MsgID Table, sch_lab_table.tbl)
+CFE_TBL_FILEDEF(SCH_LAB_ScheduleTable, SCH_LAB_APP.ScheduleTable, Schedule Lab MsgID Table, sch_lab_table.tbl)
